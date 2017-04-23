1b1c7639c6eb9dc77f5e21bd0536ac90884e9a90
Alessandro Borsoi <alessandroborsoi@MacBook-Pro.lan>
2017-04-23 16:42:35 +0200

    Commands parser

--- apps/test.c
+++ apps/test.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
 #include <upo_dir_graph.h>
 
-#define NOT_VALID -1
-#define CREATE 0
-#define DESTROY 1
-#define NUM_VERTEX 2
-#define NUM_EDGE 3
-#define DEG_IN 4
-#define DEG_OUT 5
-#define DEG_TOT 6
-#define QUIT 20
+#define MAX_ARG 3
+
+enum action_e {
+    NOT_VALID, 
+    CREATE,
+    DESTROY,
+    NUM_VERTEX,
+    NUM_EDGE,
+    DEG_IN,
+    DEG_OUT,
+    DEG_TOT,
+    QUIT
+};
+typedef enum action_e action;
+
+struct command_s {
+    action action;
+    int param1;
+    int param2;
+};
+typedef struct command_s* command_t;
 
 int quit;
 upo_dirgraph_t graph = NULL;
@@ -28,23 +40,23 @@ void print_menu()
     printf("enum \t\t\t\t Numero di archi del grafo\n");
 
     printf("TODO\n");
-    printf("din \t [vertex] \t\t Grado entrante di un vertice\n");
-    printf("dout \t [vertex] \t\t Grado uscente di un vertice\n");
-    printf("dtot \t [vertex] \t\t Grado totale di un vertice\n");
+    printf("din \t vertex \t\t Grado entrante di un vertice\n");
+    printf("dout \t vertex \t\t Grado uscente di un vertice\n");
+    printf("dtot \t vertex \t\t Grado totale di un vertice\n");
     printf("empty \t\t\t\t Controlla se il grafo e' vuoto\n");
-    printf("vadj \t [vertex] \t\t Vertici adiacenti a un vertice\n");
-    printf("eout \t [vertex] \t\t Archi uscenti da un vertice\n");
-    printf("ein \t [vertex] \t\t Archi entranti da un vertice\n");
-    printf("einc \t [vertex] \t\t Archi incidenti a un vertice\n");
+    printf("vadj \t vertex \t\t Vertici adiacenti a un vertice\n");
+    printf("eout \t vertex \t\t Archi uscenti da un vertice\n");
+    printf("ein \t vertex \t\t Archi entranti da un vertice\n");
+    printf("einc \t vertex \t\t Archi incidenti a un vertice\n");
     printf("addv \t [number] \t\t Aggiunge un numero [number] di vertici al grafo\n");
-    printf("hasv \t [vertex] \t\t Controlla se il grafo ha il dato vertice\n");
-    printf("remv \t [vertex] \t\t Rimozione del vertice\n");
-    printf("adde \t [vertex1] [vertex2] \t Aggiunge un arco che esce da [vertex1] ed entra in [vertex2]\n");
-    printf("hase \t [vertex1] [vertex2] \t Controlla se il grafo ha un arco che esce da [vertex1] ed entra in [vertex2]\n");
-    printf("reme \t [vertex1] [vertex2] \t Rimozione dell'arco che esce da [vertex1] ed entra in [vertex2]\n");
-    printf("adj? \t [vertex1] [vertex2] \t Vertici adiacenti\n");
+    printf("hasv \t vertex \t\t Controlla se il grafo ha il dato vertice\n");
+    printf("remv \t vertex \t\t Rimozione del vertice\n");
+    printf("adde \t vertex1 vertex2 \t Aggiunge un arco che esce da vertex1 ed entra in vertex2\n");
+    printf("hase \t vertex1 vertex2 \t Controlla se il grafo ha un arco che esce da vertex1 ed entra in vertex2\n");
+    printf("reme \t vertex1 vertex2 \t Rimozione dell'arco che esce da vertex1 ed entra in vertex2\n");
+    printf("adj? \t vertex1 vertex2 \t Vertici adiacenti\n");
     printf("\n");
-    printf("BFS \t [source] \t\t Visita in ampiezza del grafo\n");
+    printf("BFS \t source \t\t Visita in ampiezza del grafo\n");
     printf("DFS \t\t\t\t Visita in profondità totale del grafo\n");
     printf("cyc \t\t\t\t Controlla se il grafo contiene dei cicli\n");
     printf("DAG \t\t\t\t Controlla se un grafo graph e' un DAG\n");
@@ -57,57 +69,48 @@ void print_menu()
     printf("\n");
 }
 
-int get_action()
+action string_to_action(char* action)
 {
-    int action;
-    char input[30];
-    printf("> ");
-    scanf("%29s", input);
-    input[29] = '\0';
-    switch (input[0])
-    {
-        case 'c':
-        {  
-            action = CREATE;
-        } break;
-        case 'd':
-        {  
-            action = DESTROY;
-        } break;
-        case 'n':
-        {  
-            if (input[1] == 'v')
-                action = NUM_VERTEX;
-            else if (input[1] == 'a')
-                action = NUM_EDGE;
-            else
-                action = NOT_VALID;
-        } break;
-        case 'g':
-        {  
-            if (input[1] == 'i')
-                action = DEG_IN;
-            else if (input[1] == 'u')
-                action = DEG_OUT;
-            else if (input[1] == 't')
-                action = DEG_TOT;
-            else
-                action = NOT_VALID;
-        } break;
-        case 'q':
-        {  
-            action = QUIT;
-        } break;
-        default:
-        {
-            action = NOT_VALID;
-        }
-    }
-    return action;
+    if (strncmp(action, "create", 6) == 0)
+        return CREATE;
+    if (strncmp(action, "destroy", 7) == 0)
+        return DESTROY;
+    return NOT_VALID;
 }
 
-void execute_action(int action)
+void get_command(command_t command)
 {
+    printf("> ");
+    int input_size = 512;
+    char* input = malloc(input_size);
+    fgets(input, input_size, stdin);                        // In input viene riversato il comando
+
+    char** argv = NULL;                                     // argv conterrà l'elenco dei parametri passati
+    char* p = strtok(input, " ");                           // Si usa strtok con lo spazio come separatore
+    int n_spaces = 0;
+    while (p)                                               // Si cicla finchè ci sono parametri searati da spazi
+    {
+        argv = realloc(argv, sizeof(char*) * ++n_spaces);   // Ad ogni iterazione si rialloca lo spazio
+        if (argv == NULL)
+            exit (-1);
+        argv[n_spaces - 1] = p;                             // Si inserisce l'ultimo parametro letto
+        p = strtok(NULL, " ");
+    }
+    argv = realloc(argv, sizeof(char*) * (n_spaces + 1));  // Si alloca spazio per il terminatore
+    argv[n_spaces] = 0;
+
+    command->action = string_to_action(argv[0]);
+
+    // for (int i = 0; i < (n_spaces+1); ++i)
+        // printf("argv[%d] = %s\n", i, argv[i]);
+
+    free(argv);
+    free(input);
+}
+
+void execute_command(command_t command)
+{
+    action action = command->action;
     switch (action)
     {
         case NOT_VALID:
@@ -165,11 +168,13 @@ void execute_action(int action)
 int main()
 {
     quit = 0;
+    command_t command = malloc(sizeof(struct command_s));
     while (!quit)
     {
         print_menu();
-        int action = get_action();
-        execute_action(action);
+        get_command(command);
+        execute_command(command);
     }
+    free(command);
     return 0;
 }
